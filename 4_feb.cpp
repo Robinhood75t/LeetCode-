d08f29b456c07ec5efa178785d3aaf2e4f1db81d
Robinhood75t <161246802+Robinhood75t@users.noreply.github.com>
2025-02-04 22:59:13 +0530

    new file adding

---
+++ 4_feb.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxAscendingSum(vector<int>& nums) {
+        int n = nums.size();
+        int maxsum = 0;
+        int currsum = 0;
+        for(int i=0;i<n;i++){
+            if(i == 0 || nums[i] > nums[i-1]){
+                currsum += nums[i];
+            }else{
+                currsum = nums[i];
+            }
+            maxsum = max(currsum , maxsum);
+        }
+        return maxsum;
+    }
+};
