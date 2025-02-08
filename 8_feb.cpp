77f2bc1f994b19d3b5dd59ee630ae3f24f3c3075
Robinhood75t <161246802+Robinhood75t@users.noreply.github.com>
2025-02-08 21:45:34 +0530

    adding files

--- 8_feb.cpp
+++ 8_feb.cpp
@@ -1,1 +1,34 @@
-// todays code here 
+class NumberContainers {
+public:
+    unordered_map<int,int> map;
+    unordered_map<int,set<int>> revmap;
+    NumberContainers() {
+        
+    }
+    
+    void change(int index, int number) {
+        if(map.find(index) != map.end()){
+            int ele = map[index];
+            revmap[ele].erase(index);
+            if(revmap[ele].empty()){
+                revmap.erase(ele);
+            }
+        }
+        map[index] = number;
+        revmap[number].insert(index);
+    }
+    
+    int find(int number) {
+        if(revmap.find(number) == revmap.end() || revmap.empty()){
+            return -1;
+        }
+        return *revmap[number].begin();
+    }
+};
+
+/**
+ * Your NumberContainers object will be instantiated and called as such:
+ * NumberContainers* obj = new NumberContainers();
+ * obj->change(index,number);
+ * int param_2 = obj->find(number);
+ */
